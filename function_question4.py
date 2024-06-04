b82c63cdf06497304be397557f365e5870d8e361
Thejasri-Narayanan <125177155+Thejasri-Narayanan@users.noreply.github.com>
2024-06-04 19:30:55 +0530

    function_question4.py

---
+++ function_question4.py
@@ -0,0 +1,11 @@
+# Question 4
+# Use the get_cost() function you defined in Question 3 to calculate the cost of applying one coat of paint to a room with:
+
+# 432 square feet of walls, and
+# 144 square feet of ceiling.
+# Assume that one gallon of paint covers 400 square feet and costs $15. As in Question 3, assume you can buy partial gallons of paint. Do not round your answer.
+
+# code: (continuous to question 3)
+
+project_cost = get_cost(432,144,400,15)
+print(project_cost)
