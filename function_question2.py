94ebb92624ac91704e4fd968b31d2535f0059fd9
Thejasri-Narayanan <125177155+Thejasri-Narayanan@users.noreply.github.com>
2024-06-04 19:15:48 +0530

    Create function_question2.py

---
+++ function_question2.py
@@ -0,0 +1,20 @@
+# Question 2
+# You are thinking about buying a home and want to get an idea of how much you will spend, based on the number of bedrooms and bathrooms. You are trying to decide between four different options:
+
+# Option 1: house with two bedrooms and three bathrooms
+# Option 2: house with three bedrooms and two bathrooms
+# Option 3: house with three bedrooms and three bathrooms
+# Option 4: house with three bedrooms and four bathrooms
+# Use the get_expected_cost() function you defined in question 1 to set option_1, option_2, option_3, and option_4 to the expected cost of each option.
+
+# code : (It uses the function created from the first question, these are continuous questions that are related to one another)
+
+option_one = get_expected_cost(2,3)
+option_two = get_expected_cost(3,2)
+option_three = get_expected_cost(3,3)
+option_four = get_expected_cost(3,4)
+
+print(option_one)
+print(option_two)
+print(option_three)
+print(option_four)
