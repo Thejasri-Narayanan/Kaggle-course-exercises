cfcd5863446e51aa6516dabc1100c4f0bd82ab2b
Thejasri-Narayanan <125177155+Thejasri-Narayanan@users.noreply.github.com>
2024-06-04 23:18:46 +0530

    Create datatype_question4.py

---
+++ datatype_question4.py
@@ -0,0 +1,10 @@
+# Question 4
+# We'll continue our study of boolean arithmetic. For this question, your task is to provide a description of what happpens when you add booleans
+
+# code:
+
+print(False + False)
+print(True + False)
+print(False + True)
+print(True + True)
+print(False + True + True + True)
