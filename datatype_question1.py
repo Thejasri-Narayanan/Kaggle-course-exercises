65447a8629e49d04047b98d9325f175311a20bdf
Thejasri-Narayanan <125177155+Thejasri-Narayanan@users.noreply.github.com>
2024-06-04 23:10:33 +0530

    Create datatype_question1.py

---
+++ datatype_question1.py
@@ -0,0 +1,14 @@
+# Question 1
+# You have seen how to convert a float to an integer with the int function. Try this out yourself by running the code cell below.
+
+# code:
+
+# Define a float
+y = 1.
+print(y)
+print(type(y))
+
+# Convert float to integer with the int function
+z = int(y)
+print(z)
+print(type(z))
